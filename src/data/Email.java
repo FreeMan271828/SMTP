bf3c2360896872b745b07fb1bd918f93caef011d
FreeMan <1114509193@qq.com>
2024-05-14 16:29:43 +0800

    add data

---
+++ src/data/Email.java
@@ -0,0 +1,65 @@
+package data;
+
+public class Email {
+
+    private Integer id;
+
+    private Integer senderId;
+
+    private Integer receiverId;
+
+    private String subject;
+
+    private String content;
+
+    private String tip;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+}
